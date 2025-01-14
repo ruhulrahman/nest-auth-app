d271ff4eab80f87a6e1ab0f017dc61200622ec39
ruhulrahman2233 <ruhulrahman2233@gmail.com>
2025-01-14 23:35:24 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory, } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
+import 'dotenv/config';
 import { AppModule } from './app.module';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
