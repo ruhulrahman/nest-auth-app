e379fcd11a9d3c2aa68c20e5f44f8835d66bfbc4
ruhulrahman <ruhulrahman2233@gmail.com>
2025-02-18 12:40:18 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser()); // Enable cookie parsing
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/models/designation.entity.ts
+++ src/models/designation.entity.ts
@@ -7,4 +7,43 @@ export class Designation {
 
   @Column()
   name: string;
+  
+  @Column()
+  description: string;
+
+  @Column()
+  isActive: boolean;
+  
+  @Column({ default: false })
+  isDeleted: boolean;
+  
+  @Column({ default: null })
+  deletedAt: Date;
+  
+  @Column({ default: null })
+  createdAt: Date;
+  
+  @Column({ default: null })
+  updatedAt: Date;
+  
+  @Column({ default: null })
+  deletedBy: number;
+  
+  @Column({ default: null })
+  createdBy: number;
+  
+  @Column({ default: null })
+  updatedBy: number;
+  
+  @Column({ default: null })
+  deletedByUserId: number;
+  
+  @Column({ default: null })
+  createdByUserId: number;
+  
+  @Column({ default: null })
+  updatedByUserId: number;
+
+  @Column({ default: null })
+  departmentId: number;
 }
